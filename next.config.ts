ff2266429750392c87a083a4a1507a06a56f5f22
sum37 <82599165+sum37@users.noreply.github.com>
2025-02-01 15:13:34 +0900

    next.config.ts assetPrefix 수정

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,10 @@ const nextConfig: NextConfig = {
     unoptimized: true, // 이미지 최적화 기능 비활성화
   },
   basePath: '/lunatic_front_rebase',
-  assetPrefix: '/lunatic_front_rebase/',
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://sum37.github.io/lunatic_front_rebase"
+      : "",
 };
-
+ßß
 export default nextConfig;
