ee01c1183d5e31a26e8d8d8de811bcbabcc765bb
sum37 <82599165+sum37@users.noreply.github.com>
2025-02-01 14:53:59 +0900

    edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // 이미지 최적화 기능 비활성화
   },
+  basePath: '/lunatic_front_rebase',
+  assetPrefix: '/lunatic_front_rebase/',
 };
 
 export default nextConfig;
