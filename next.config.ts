38a00efe5e56d5bd257fa560ed24a7bf74342366
sum37 <82599165+sum37@users.noreply.github.com>
2025-02-04 13:47:12 +0900

    assetPrefix 한번더 수정

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    unoptimized: true, // 이미지 최적화 기능 비활성화
+    unoptimized: true, // GitHub Pages에서 이미지 최적화 비활성화
   },
   basePath: "/lunatic_front_rebase",
-  assetPrefix: "/lunatic_front_rebase/", // GitHub Pages에서 _next/static/이 올바르게 동작하도록 설정
-  trailingSlash: true, // HTML 파일 확장자 처리
+  assetPrefix: "https://sum37.github.io/lunatic_front_rebase/",
+  trailingSlash: true, // GitHub Pages에서 정적 파일 경로 문제 해결
 };
 
 export default nextConfig;
