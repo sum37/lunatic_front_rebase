4257d5a06ad74d234c16011f1960e9f8a1874222
sum37 <82599165+sum37@users.noreply.github.com>
2025-02-04 13:29:21 +0900

    header log trophy 경로 수정

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   images: {
     unoptimized: true, // 이미지 최적화 기능 비활성화
   },
-  basePath: '/lunatic_front_rebase',
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://sum37.github.io/lunatic_front_rebase/"
-      : "",
+  basePath: "/lunatic_front_rebase",
+  assetPrefix: "/lunatic_front_rebase/", // GitHub Pages에서 _next/static/이 올바르게 동작하도록 설정
+  trailingSlash: true, // HTML 파일 확장자 처리
 };
+
 export default nextConfig;
